bf9bdca264de62c963dc28e8c43c5c2d4d6c023a
@programadorguevara <85507672+MaxijGuevara@users.noreply.github.com>
2022-05-04 19:18:38 -0300

    post delete

--- routes.js
+++ routes.js
@@ -13,4 +13,27 @@ routes.get('/', (req, res)=>{
     })
 })
 
+routes.post('/', (req, res)=>{
+    req.getConnection((err, conn)=>{
+        if(err) return res.send(err)
+
+        conn.query('INSERT id_reclamo, fk_denunciante_reclamo, fecha_hora_reclamo, contenido_reclamo, tipo_reclamo, calle_reclamo, numero_reclamo, orientacion_reclamo, lat_reclamo, lng_reclamo, imagen, operador, estado_reclamo, fk_origen, fk_origen_padre, link_redes_sociales, fk_prioridad, eliminado, fecha_hora_eliminado, fecha_hora_finalizado INTO db_sem.dbo_reclamos', [req.body],(err, rows)=>{
+            if(err) return res.send(err)
+
+            res.send('reclamo añadido con exito')
+        })
+    })
+})
+
+routes.delete('/:id_reclamo', (req, res)=>{
+    req.getConnection((err, conn)=>{
+        if(err) return res.send(err)
+
+        conn.query('DELETE id_reclamo, fk_denunciante_reclamo, fecha_hora_reclamo, contenido_reclamo, tipo_reclamo, calle_reclamo, numero_reclamo, orientacion_reclamo, lat_reclamo, lng_reclamo, imagen, operador, estado_reclamo, fk_origen, fk_origen_padre, link_redes_sociales, fk_prioridad, eliminado, fecha_hora_eliminado, fecha_hora_finalizado FROM db_sem.dbo_reclamos', [req.body],(err, rows)=>{
+            if(err) return res.send(err)
+
+            res.send('reclamo borrado con exito')
+        })
+    })
+})
 module.exports = routes

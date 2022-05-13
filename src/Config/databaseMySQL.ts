efab9d39718f6f0fc705c99783acc3738ac45b63
@programadorguevara <85507672+MaxijGuevara@users.noreply.github.com>
2022-05-13 16:39:58 -0300

    ConfigSQL

---
+++ src/Config/databaseMySQL.ts
@@ -0,0 +1,16 @@
+var mysql  = require('mysql');
+var dbMysql = mysql.createConnection({
+  host     : '192.168.5.152',
+  user     : 'kevinm',
+  password : 'Kevin.2021',
+  database : 'db_sem'
+});
+ 
+dbMysql.connect(function (err){
+    if(err){
+        throw err;
+    }else{
+        console.log("CONEXION EXITOSA");
+    }
+});
+export default dbMysql;
